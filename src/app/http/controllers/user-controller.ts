0b43d35584124f4a02c4461b20fd6ce780c69bff
nizniz <nizar.chaouachi@gmail.com>
2019-06-10 21:03:48 +0200

    Fix knex scripts, introduce try catch

--- src/app/http/controllers/user-controller.ts
+++ src/app/http/controllers/user-controller.ts
@@ -1,18 +1,27 @@
-import { Request, Response } from 'express';
-import { User } from '../../models/user';
+import {Request, Response} from 'express';
+import {User} from '../../models/user';
+import {HTTP404Error} from '../../lib/http-client-errors';
 
 class UserController {
   constructor() {
   }
 
   index = async (req: Request, res: Response) => {
-    const users = await User.query();
-    return res.status(200).json(users);
+    try {
+      const users = await User.query();
+      return res.status(200).json(users);
+    } catch (error) {
+      throw new HTTP404Error(`Users Not Found${error.toString()}`);
+    }
   }
 
   show = async (req: Request, res: Response) => {
-    const user = await User.query().findById(req.params.id);
-    return res.status(200).json(user);
+    try {
+      const user = await User.query().findById(req.params.id);
+      return res.status(200).json(user);
+    } catch (error) {
+      throw new HTTP404Error(`User Not Found${error.toString()}`);
+    }
   }
 }
 
